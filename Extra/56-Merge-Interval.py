78aa4b6d9143bd436acf810bd5954ffb01cb9b20
cineali <vodbwi@gmail.com>
2025-10-24 13:40:46 +0300

    Description added

--- Extra/56-Merge-Interval.py
+++ Extra/56-Merge-Interval.py
@@ -1,3 +1,8 @@
+'''
+Author: Ali Çine
+Description:
+    First we sort the list to ensure that first elements are in ascending order. Then we loop every element in list with while loop. Because of later merges we must also compare new merge with next element from list, for that i use the last item (output[-1]) after checking that output list has valid length. If new merged list (output[-1]) doesnt overlap with item then i simply append item.
+'''
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         n = len(intervals)
@@ -14,11 +19,3 @@ class Solution:
                     output.append(intervals[i])
             i+= 1
         return output
-
-    def hasNext(self, intervals, i):
-        try:
-            intervals[i+1]
-        except:
-            return False
-        else:
-            return True
