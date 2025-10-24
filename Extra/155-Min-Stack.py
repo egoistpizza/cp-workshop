24e0620037426bb56b460e98f5bfbab300f55ec1
cineali <vodbwi@gmail.com>
2025-10-24 13:31:46 +0300

    description added

--- Extra/155-Min-Stack.py
+++ Extra/155-Min-Stack.py
@@ -1,3 +1,8 @@
+'''
+Author: Ali Çine
+Description:
+    For the 155.Question we dont create a new data structure, we only implement "getMin" function to appropriate "list" data structure.To calculate the minimum element at O(1) time complexity, we must return a variable that is ready in memory. We check in every push if minimum element has changed and update minstack. We use list for minstack because of pop function, if we didnt use a list then we wouldnt get previous minimum if new minimum was popped.
+'''
 class MinStack:
     def __init__(self):
         self.stack = []
